3c46968764bc0aeae5e02d5b5231378c172cb012
Florins13 <sofinetflorin@gmail.com>
2021-01-31 02:53:45 +0000

    Divide commands to files

--- bot.js
+++ bot.js
@@ -1,7 +1,20 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {prefix, discordtoken, riottoken} = require('./config.json')
-const fetch = require('node-fetch');
+
+const {prefix, discordtoken} = require('./config.json');
+
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 
 client.on('ready', () => {
@@ -10,48 +23,26 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content.startsWith(`${prefix}ping`)) {
-        message.channel.send('Pong.');
-    } else if (message.content === `${prefix}beep`) {
-        message.channel.send('Boop.');
-    }
-    else if (message.content === `${prefix}server`) {
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+        client.commands.get('ping').execute(message);
     }
 });
 
 client.on('message', async message => {
     if (message.channel.id === "804494561556037643"){
         if (message.content.startsWith(`${prefix}rank`)) {
-            const options = {
-                method: 'GET',
-                headers: {
-                    'content-type': 'application/json',
-
-                }
-            };
-            let message_content = message.content.slice(6);
-            if(/\s/.test(message_content)){
-                message_content = message_content.replace(/\s/g, '%20')
-            }
-            // message.channel.send(message_content);
-            let getAccountId = new URL('https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + message_content + '?api_key=' + riottoken)
-            console.log(getAccountId)
-            let response = await fetch(getAccountId);
-            let json = await response.json();
-            console.log(json)
-            let getRank = 'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+ json.id +'?api_key=' + riottoken;
-            let secondresponse = await fetch(getRank);
-            let secondjson = await secondresponse.json();
-            console.log(secondjson);
-
-            message.channel.send(secondjson[0].tier + ' , ' + secondjson[0].rank + ' , Meciuri castigate: ' + secondjson[0].wins + ', Meciuri pierdute: ' + + secondjson[0].losses);
+            client.commands.get('lol-rank').execute(message);
        }
     }
 });
 client.on('message', async message => {
 	// Join the same voice channel of the author of the message
 	if (message.member.voice.channel) {
-		const connection = await message.member.voice.channel.join();
+        const connection = await message.member.voice.channel.join();
+        // const audio = connection.receiver.createStream(user, { mode: 'pcm' });
+        // audio.pipe(fs.createWriteStream('user_audio'));
+        // const audio = connectionA.receiver.createStream('238273480594817024');
+        // connectionB.play(audio, { type: 'opus' });
+        // console.log(audio)
 	}
 });
 
---
+++ commands/lol-rank.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+const {riottoken} = require('../config.json');
+
+module.exports = {
+	name: 'lol-rank',
+	description: 'Get lol ranks by given Summoner Name',
+	async execute(message, args) {
+        let message_string = message.content.slice(6);
+        if(/\s/.test(message_string)){
+            message_string = message_string.replace(/\s/g, '%20')
+        }
+        try{
+            let getAccountId = new URL('https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + message_string + '?api_key=' + riottoken);
+            console.log(getAccountId)
+            let response = await fetch(getAccountId);
+            let json = await response.json();
+            let getRank = 'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+ json.id +'?api_key=' + riottoken;
+            let secondresponse = await fetch(getRank);
+            let secondjson = await secondresponse.json();
+            message.channel.send(secondjson[0].tier + ' , ' + secondjson[0].rank + ' , Meciuri castigate: ' + secondjson[0].wins + ', Meciuri pierdute: ' + + secondjson[0].losses);
+        }
+        catch(error){
+            message.channel.send("Esti unranked fratelo!");
+        }       
+	},
+};
