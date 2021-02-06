002e8a56916f81f50754bfbb42db5df8962446e1
Florins13 <sofinetflorin@gmail.com>
2021-02-06 18:14:56 +0000

    add regions on check

--- commands/lol-rank.js
+++ commands/lol-rank.js
@@ -2,26 +2,57 @@ const fetch = require('node-fetch');
 const {riottoken} = require('../config.json');
 const embedMsg = require('../embeds/lol-embed.js');
 
+const servers = {
+    "euw": 'EUW1',
+    "eune": 'EUN1',
+    "na": 'NA1',
+    "kr": 'KR',
+    "br": 'BR1',
+    "jp": 'JP1',
+    "las": 'LA1',
+    "oce": 'OC1',
+    "tr": 'TR1',
+    "ru": 'RU',
+};
 module.exports = {
 	name: 'lol-rank',
 	description: 'Get lol ranks by given Summoner Name',
 	async execute(message, args) {
+        const getAPIregion = (string) => {
+            for (const [key, value] of Object.entries(servers)) {
+                switch(string) {
+                    case key:
+                        return servers[string];
+                    default:
+                        console.log("SHIT");
+                };
+            }
+        };
         let user = message.content.slice(6);
-        const summonerName = message.content.slice(6);
-        if(/\s/.test(user)){
-            user = user.replace(/\s/g, '%20')
+        let summonerName = message.content.slice(6);
+        let opGG = 'eune';
+        let region = getAPIregion(opGG);
+        if(message.content.charAt(5) === '$'){
+            opGG = message.content.slice(6, message.content.indexOf(' '));
+            region = getAPIregion(opGG);
+            user = message.content.slice(message.content.indexOf(' ')+1);
+            summonerName =  message.content.slice(message.content.indexOf(' ')+1)
+            if(/\s/.test(user)){
+                user = user.replace(/\s/g, '%20')
+            }
         }
         try{
-            const getAccountId = new URL('https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + user + '?api_key=' + riottoken);
+            const getAccountId = new URL(`https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${user}?api_key=${riottoken}`); // string literal puts 20% instead of space, omg...
             let response = await fetch(getAccountId);
             let accountData = await response.json();
-            const getRank = 'https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/'+ accountData.id +'?api_key=' + riottoken;
+            const getRank = `https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${accountData.id}?api_key=${riottoken}`;
             let secondresponse = await fetch(getRank);
             let summonerData = await secondresponse.json();
-            message.channel.send(embedMsg.render(summonerName, user, summonerData[0].tier, summonerData[0].rank, summonerData[0].leaguePoints, summonerData[0].wins, summonerData[0].losses, summonerData[0].queueType.replace(/_/g, ' ')))
+            message.channel.send(embedMsg.render(summonerName, user, summonerData[0], opGG));
         }
         catch(error){
+            // console.log(error);
             message.channel.send("Esti unranked fratelo!");
-        }       
+        };        
 	},
 };
--- embeds/lol-embed.js
+++ embeds/lol-embed.js
@@ -3,27 +3,28 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'Embed for lol',
 	description: 'Give fancy window for given summoner!',
-	render(sumonnerName, user,league, rank, leaguePoints, wins, losses, queueType) {
+	render(name, user, summoner, region) {
 		const exampleEmbed = new Discord.MessageEmbed()
-		.attachFiles(['./assets/ranked-emblems/' + league + '.png'])
+		.attachFiles(['./assets/ranked-emblems/' + summoner.tier + '.png'])
 		.attachFiles(['./assets/icons/skull.png'])
 		.setColor("ORANGE")
-		.setTitle(sumonnerName)
-		.setURL('https://eune.op.gg/summoner/userName=' + user)
+		.setTitle(name)
+		.setURL(`https://${region==='kr'?'kr.':region+'.'}op.gg/summoner/userName=${user}`)
 		// .setAuthor('Rankul acestui noob este: ', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
 		// .setDescription('Some description here')
-		.setThumbnail('attachment://' + league + '.png')
+		.setThumbnail('attachment://' + summoner.tier + '.png')
 		.addFields(
-			{ name: 'Liga:', value: league + ' ' + rank, inline : false },
-			{ name: 'Tip q:', value: queueType , inline: false },
-			{ name: 'Puncte in liga:', value: '---- ' + leaguePoints  + ' ----' , inline: false },
-			{ name: 'Meciuri castigate:', value: '---- ' + wins + ' ----' , inline: false },
-			{ name: 'Meciuri pierdute:', value: '---- ' + losses + ' ----' , inline: false },
-			{ name: 'Procentaj castigate:', value: '--- ' + Math.round((wins/(wins+losses))*100) + '%' + ' ---' , inline: false },
+			{ name: 'Liga:', value: summoner.tier + ' ' + summoner.rank, inline : false },
+			{ name: 'Regiune:', value: region.toUpperCase(), inline : false },
+			{ name: 'Tip q:', value: summoner.queueType.replace(/_/g, ' '), inline: false },
+			{ name: 'Puncte in liga:', value: '---- ' + summoner.leaguePoints  + ' ----' , inline: false },
+			{ name: 'Meciuri castigate:', value: '---- ' + summoner.wins + ' ----' , inline: false },
+			{ name: 'Meciuri pierdute:', value: '---- ' + summoner.losses + ' ----' , inline: false },
+			{ name: 'Procentaj castigate:', value: '--- ' + Math.round((summoner.wins/(summoner.wins+summoner.losses))*100) + '%' + ' ---' , inline: false },
 			
 		)
 		.setTimestamp()
-		.setFooter('Powered by Hackerman v1.0', 'attachment://skull.png');
+		.setFooter('Powered by Je v1.0', 'attachment://skull.png');
 		
 		return exampleEmbed;
 	},
